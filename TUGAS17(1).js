569e590a625e23ec69795878ad5ed1cd39c0cd06
faizanshar <muhammadfaizanshar@gmai.com>
2020-09-10 22:22:27 +0700

    first commit

---
+++ TUGAS17(1).js
@@ -0,0 +1,6 @@
+var listDesaBantul = [
+    ['argodadi,sedayu','argomulyo,sedayu','argorejo,sedayu','argosari'],
+    ['bangunjiwo,kasihan','banguntapan,banguntapan','bantul,bantul','baturetno,banguntapan','bawuran,pleret'],
+    ['canden,jetis','caturharjo,pandak','cetan srigandi']
+]
+console.log(listDesaBantul[0][0])
